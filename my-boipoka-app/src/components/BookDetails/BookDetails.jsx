7dac01b60b247b8dccfb42403584b18ed47610d1
Rhcoder79 <robiulhasan7972171@gmail.com>
2026-01-14 23:46:01 +0600

    80% module complete

--- my-boipoka-app/src/components/BookDetails/BookDetails.jsx
+++ my-boipoka-app/src/components/BookDetails/BookDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import { addToStoredDB } from '../../utility/addToDB';
 
 const BookDetails = () => {
     const {id}=useParams();
@@ -10,11 +11,21 @@ const bookId=parseInt(id);
     const singleBook=data.find(book=>book.bookId===bookId);
     // console.log(singleBook);
     // console.log(typeof id,data);
-   const {bookName,image} =singleBook;
+   const {bookName,image} =singleBook || {};
+    const handleMarkAsRead=id=>{
+        //store with id
+        //where to store
+        //array or collection
+        //if book already exist then show a alert
+        // if book not exist then push in the collection or array
+          addToStoredDB(id)
+    }
     return (
-        <div>
+        <div className=' w-2/3 mx-auto'>
          <img className='w-48' src={image} alt="" />
          <h4>{bookName}</h4>
+         <button onClick={()=>handleMarkAsRead(id)} className='btn btn-accent m-2'>Mar as Read</button>
+         <button className='btn btn-info m-2'>Add To WishList</button>
         </div>
     );
 };
