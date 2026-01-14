4438a9b16992e013e2469bd7f03e452f7e78894f
Rhcoder79 <robiulhasan7972171@gmail.com>
2026-01-14 16:29:50 +0600

    add

--- my-boipoka-app/src/main.jsx
+++ my-boipoka-app/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import { router } from './Routes/routes.jsx';
 
 
 createRoot(document.getElementById('root')).render(
